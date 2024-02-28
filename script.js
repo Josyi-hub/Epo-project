7a46065165e0e8595d73ef2023888f0339204eff
Temperance <astridenare@gmail.com>
2024-02-28 17:14:31 +0000

    Ajout de la section FAQ

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
 document.getElementById('welcomeButton').addEventListener('click', function() { 
 document.getElementById('welcomeMessage').textContent = 'Nous sommes ravis de vous accueillir sur le site dédié à l\'EPO. Explorez nos programmes et nos initiatives!'; 
 }); 
+// Script pour dynamiser la section FAQ
+document.addEventListener('DOMContentLoaded', function() {
+    console.log('Bienvenue dans la section FAQ de l\'École Polytechnique de Ouagadougou!');
+    });
